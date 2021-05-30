310b556a215cb11157965c820d1577465bf8566b
zzh-1992 <ezzh1992@icloud.com>
2021-05-30 22:28:57 +0800

    手动提交/回滚事务demo

--- springboot/src/main/java/com/grapefruit/springboot/mysql/Service/MyServiceImpl.java
+++ springboot/src/main/java/com/grapefruit/springboot/mysql/Service/MyServiceImpl.java
@@ -7,12 +7,17 @@ package com.grapefruit.springboot.mysql.Service;
 import com.grapefruit.springboot.mysql.Dao.MyDao;
 import com.grapefruit.springboot.mysql.entity.Grape;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 /**
- * 相关描述
+ * service
  *
  * @author zhihuangzhang
  * @version 1.0
@@ -21,9 +26,12 @@ import java.util.List;
 @Service
 public class MyServiceImpl implements MyService {
     @Autowired
-    MyDao myDao;
+    private MyDao myDao;
 
-    //@Transactional
+    @Autowired
+    private DataSourceTransactionManager tx;
+
+    //@Transactional(rollbackFor = Exception.class)
     @Override
     public Grape selectGrapeById(int id) {
         return myDao.selectGrapeById(id);
@@ -34,9 +42,32 @@ public class MyServiceImpl implements MyService {
         return myDao.insert(grape);
     }
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public int insertList(List<Grape> list) {
-        return myDao.insertList(list);
+        // 1.获取事务定义
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        // 2.设置事务隔离级别，开启新事务
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        // 3.获得事务状态
+        TransactionStatus status = tx.getTransaction(def);
+
+        int i = 0;
+        try {
+            i = myDao.insertList(list);
+
+            // 手动触发异常;
+            int j = 1 / 0;
+
+            // 手动提交事务
+            tx.commit(status);
+        } catch (Exception e) {
+
+            // 手动回滚事务
+            tx.rollback(status);
+            e.printStackTrace();
+        }
+        return i;
     }
 
     @Override
