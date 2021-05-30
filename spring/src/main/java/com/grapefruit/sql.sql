442104927ea63583adf0c463b152b3678be41f7f
zzh-1992 <ezzh1992@icloud.com>
2021-05-30 13:02:43 +0800

    添加官网sql教程

---
+++ spring/src/main/java/com/grapefruit/sql.sql
@@ -0,0 +1,83 @@
+create database grapefruit;
+
+use grapefruit;
+
+# 创建表
+create table grape
+(
+    id int primary key ,
+    name varchar(20),
+    num bigint,
+    content text,
+    index i(name)
+);
+
+#修改表属性
+alter table grape modify num bigint signed;
+
+#ALTER TABLE attence CHANGE attence_name NAME  VARCHAR(20)
+ALTER  TABLE grape MODIFY column name varchar(19) default 'orange' comment '名称';
+
+insert into grape (id,name,num,content) values (1,'meizhou',2147483649,'just bigint test');
+insert into grape (id,name,num,content) values (2,'shenzhen',2147483650,'just bigint test 0521');
+insert into grape (id,name,num,content) values (3,'meizhou',2147483651,'just bigint test 0532');
+insert into grape (id,name,num,content) values (4,'meizhou',2147483652,'just bigint test 0555');
+insert into grape (id,name,num,content) values (5,'meizhou',2147483653,'just bigint test 0566');
+
+
+select * from information_schema.columns
+where table_schema= 'grapefruit';
+
+# 查看字段属性
+show columns from grape;
+#======================官网教程==================
+# 官网链接 https://dev.mysql.com/doc/refman/8.0/en/alter-table.html
+#修改表到引擎
+ALTER TABLE t1 ENGINE = InnoDB;
+#将InnoDB表更改为使用压缩行存储格式：
+ALTER TABLE t1 ROW_FORMAT = COMPRESSED;
+#重置当前的自动增量值
+ALTER TABLE t1 AUTO_INCREMENT = 13;
+#更改默认表字符集
+ALTER TABLE t1 CHARACTER SET = utf8;
+# 添加（或更改）表格注释：
+ALTER TABLE t1 COMMENT = 'New table comment';
+
+# 这个语句还不清楚
+ALTER TABLE t1 COMMENT = "NDB_TABLE=READ_BACKUP=0,PARTITION_BALANCE=FOR_RA_BY_NODE";
+
+# 进入容器内部 docker exec -it 17 /bin/bash
+
+#更改列以更改其名称和定义
+ALTER TABLE t1 CHANGE a b BIGINT NOT NULL;
+#更改列定义但不更改其名称
+ALTER TABLE t1 CHANGE b b INT NOT NULL;
+#MODIFY 在不更改名称的情况下更改定义更方便，因为它只需要列名一次：
+ALTER TABLE t1 MODIFY b INT NOT NULL;
+#要更改列名称但不更改其定义，请使用 CHANGE或RENAME COLUMN。使用 时CHANGE，语法需要列定义，因此要保持定义不变，您必须重新指定列当前具有的定义。例如，要重命名INT NOT NULL的列 b到a，这样做：
+ALTER TABLE t1 CHANGE b a INT NOT NULL;
+#RENAME COLUMN 在不改变定义的情况下更方便地更改名称，因为它只需要旧名称和新名称：
+ALTER TABLE t1 RENAME COLUMN b TO a;
+
+##
+-- 交换字段名 swap a and b
+ALTER TABLE t1 RENAME COLUMN a TO b,
+    RENAME COLUMN b TO a;
+-- 替换字段名 "rotate" a, b, c through a cycle
+ALTER TABLE t1 RENAME COLUMN a TO b,
+    RENAME COLUMN b TO c,
+    RENAME COLUMN c TO a;
+
+#仅打算更改INT为 BIGINT：
+ALTER TABLE t1 MODIFY col1 BIGINT;
+#这种说法从改变数据类型INT 来BIGINT，但它也下降了 UNSIGNED，DEFAULT和 COMMENT属性。要保留它们，语句必须明确包含它们：
+ALTER TABLE t1 MODIFY col1 BIGINT UNSIGNED DEFAULT 1 COMMENT 'my column';
+
+#要将表默认字符集和所有字符列 ( CHAR, VARCHAR, TEXT) 更改为新字符集(慎用!!!)
+ALTER TABLE tbl_name CONVERT TO CHARACTER SET charset_name;
+#MODIFY更改单个列
+ALTER TABLE t MODIFY latin1_text_col TEXT CHARACTER SET utf8;
+ALTER TABLE t MODIFY latin1_varchar_col VARCHAR(M) CHARACTER SET utf8;
+
+#仅更改表的默认字符集
+ALTER TABLE tbl_name DEFAULT CHARACTER SET charset_name;
