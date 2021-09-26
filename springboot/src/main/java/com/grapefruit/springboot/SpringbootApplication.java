a6241536f272c11c8eb5c813466439370057dfb6
zzh-1992 <ezzh1992@icloud.com>
2021-09-26 20:19:51 +0800

    使用nacos做服务的配置中心

--- springboot/src/main/java/com/grapefruit/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/grapefruit/springboot/SpringbootApplication.java
@@ -1,7 +1,9 @@
 package com.grapefruit.springboot;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 入口主程序
@@ -9,8 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author grapefruit
  * @date 2021/05/29
  */
+
 @SpringBootApplication()
-// @MapperScan(basePackages = "com.grapefruit.springboot.mysql.mapper")
+//@MapperScan(basePackages = "com.grapefruit.springboot.mysql.mapper") /或是使用注解@Mapper
+@EnableTransactionManagement
+@NacosPropertySource(dataId = "grape",groupId = "dev_group",autoRefreshed = true)
 public class SpringbootApplication {
 
   public static void main(String[] args) {
